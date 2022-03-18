2be3824eb8c8aa74eb194b264893a016e592b78d
Frank Young <frank.young@chargepoint.com>
2022-03-18 13:50:29 -0700

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,59 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
-import dts from "rollup-plugin-dts";
+import ts from 'typescript';
+import path from 'path';
+import { readFileSync } from 'fs';
 
-const packageJson = require("./package.json");
+import json from '@rollup/plugin-json';
+import img from '@rollup/plugin-image';
+
+import babel from '@rollup/plugin-babel';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
+
+const packageJson = require('./package.json');
 
 export default [
   {
-    input: "src/index.tsx",
+    input: 'src/index.tsx',
     inlineDynamicImports: true,
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
+        format: 'cjs',
         sourcemap: true,
       },
       {
         file: packageJson.module,
-        format: "esm",
+        format: 'esm',
         sourcemap: true,
       },
     ],
     plugins: [
       peerDepsExternal(),
       resolve(),
+      json(),
+      img(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      babel({ babelHelpers: 'bundled' }),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
     ],
   },
   {
-    input: "dist/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "es" }],
-    plugins: [dts()],
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [
+      dts({
+        compilerOptions: {
+          paths: ts.readConfigFile(
+            path.resolve(__dirname, 'tsconfig.json'),
+            (p) => readFileSync(p, 'utf8')
+          ).config.compilerOptions.paths,
+        },
+      }),
+    ],
   },
 ];
