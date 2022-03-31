16a9fa1cf4864cc119f737d834a247cdcb35580f
Frank Young <77019975+fyoung-cpi@users.noreply.github.com>
2022-03-30 21:43:26 -0700

    fixed getYAxisDomain (#8)

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  preset: "ts-jest/presets/js-with-ts",
-  testEnvironment: "jsdom",
-  testMatch: ["**/*.test.*"],
-  testPathIgnorePatterns: ["./node_modules/"],
-  roots: ["<rootDir>/src"],
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/*.test.*'],
+  testPathIgnorePatterns: ['./node_modules/'],
+  roots: ['<rootDir>/src'],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.tsx?$': 'ts-jest',
   },
   moduleNameMapper: {
-    "@common/(.*)$": "<rootDir>/src/common/$1",
-    "@components/(.*)$": "<rootDir>/src/components/$1",
-    "@models/(.*)$": "<rootDir>/src/models/$1",
-    "@services/(.*)$": "<rootDir>/src/services/$1",
+    '@common/(.*)$': '<rootDir>/src/common/$1',
+    '@components/(.*)$': '<rootDir>/src/components/$1',
+    '@models/(.*)$': '<rootDir>/src/models/$1',
+    '@services/(.*)$': '<rootDir>/src/services/$1',
   },
   globals: {
-    "ts-jest": {
+    'ts-jest': {
       isolatedModules: true,
       // turn off for now
       diagnostics: false,
