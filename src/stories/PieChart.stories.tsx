d5901c4535299003bc6e0758cb721ed368b04a6b
Frank Young <frank.young@chargepoint.com>
2022-08-29 22:24:01 -0700

    added donut chart

--- src/stories/PieChart.stories.tsx
+++ src/stories/PieChart.stories.tsx
@@ -1,13 +1,6 @@
 import { scaleOrdinal } from 'd3-scale';
 import { schemeCategory10 } from 'd3-scale-chromatic';
-import {
-  ResponsiveContainer,
-  PieChart,
-  Pie,
-  Cell,
-  Tooltip,
-  Legend,
-} from 'recharts';
+import { ResponsiveContainer, PieChart, Pie, Cell, Tooltip, Legend, Label } from 'recharts';
 import { CPChartColors } from '../common/theme';
 import { ChartWrapper, TooltipContainer } from '../components/CPChartElements';
 
@@ -32,7 +25,7 @@ const renderTooltip = (props) => {
       <TooltipContainer>
         <div>
           <div>{item.name}</div>
-          <b className="value">${item.value}</b>
+          <b className='value'>${item.value}</b>
         </div>
       </TooltipContainer>
     );
@@ -48,19 +41,19 @@ export const PieChartWithLabels = () => {
 
   return (
     <ChartWrapper width={400}>
-      <ResponsiveContainer width="100%" height="100%">
+      <ResponsiveContainer width='100%' height='100%'>
         <PieChart width={400} height={400}>
           <Tooltip content={renderTooltip} />
           <Pie
             data={data}
-            cx="50%"
-            cy="50%"
-            nameKey="name"
+            cx='50%'
+            cy='50%'
+            nameKey='name'
             label={(entry) => entry.name}
             labelLine
             outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
+            fill='#8884d8'
+            dataKey='value'
           >
             {data.map((entry, index) => (
               // eslint-disable-next-line react/no-array-index-key
@@ -80,19 +73,50 @@ export const PieChartWithLegend = () => {
 
   return (
     <ChartWrapper width={400}>
-      <ResponsiveContainer width="100%" height="100%">
+      <ResponsiveContainer width='100%' height='100%'>
         <PieChart width={400} height={400}>
-          <Legend align="right" layout="vertical" verticalAlign="middle" />
+          <Legend align='right' layout='vertical' verticalAlign='middle' />
           <Pie
             data={data}
-            cx="50%"
-            cy="50%"
-            nameKey="name"
+            cx='50%'
+            cy='50%'
+            nameKey='name'
             label={(entry) => entry.value}
             labelLine
             outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
+            fill='#8884d8'
+            dataKey='value'
+          >
+            {data.map((entry, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <Cell key={`cell-${index}`} fill={domain(entry.name)} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </ChartWrapper>
+  );
+};
+
+export const Donut = () => {
+  const names = data.map((d) => d.name);
+  const scale = scaleOrdinal(Object.values(CPChartColors).reverse());
+  const domain = scale.domain(names);
+
+  return (
+    <ChartWrapper width={400}>
+      <ResponsiveContainer width='100%' height='100%'>
+        <PieChart width={400} height={400}>
+          <Tooltip content={renderTooltip} />
+          <Pie
+            data={data}
+            cx='50%'
+            cy='50%'
+            nameKey='name'
+            innerRadius={50}
+            outerRadius={80}
+            fill='#8884d8'
+            dataKey='value'
           >
             {data.map((entry, index) => (
               // eslint-disable-next-line react/no-array-index-key
